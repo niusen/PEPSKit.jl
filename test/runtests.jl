471ca5000344738c501d023ac2358f839f452105
lkdvos <lukas.devos@ugent.be>
2022-09-05 14:38:29 +0200

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,10 @@
-using PEPSKit,MPSKit,TensorKit,Test,TestExtras
+using PEPSKit, MPSKit, TensorKit
+
+peps = InfinitePEPS(2, 3)
+tpeps = InfiniteTransferPEPS(peps, 1, 1)
+
+mps = initializeMPS(tpeps, 4);
+
+mps, _, _ = leading_boundary(mps, tpeps, VUMPS());
+
+expectation_value(
